79a0e1c3b5f7174a5069e40ff86de8a22e996ab6
Onix <gotnix13@gmail.com>
2021-07-25 15:31:29 -0400

    Contacts context baseline

---
+++ client/src/context/contact/ContactContext.js
@@ -0,0 +1,6 @@
+// Initializer for contacts context
+import { createContext } from "React";
+
+const contactContext = createContext();
+
+export default contactContext;
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,7 @@
+// Use reducer gives access to state and dispatch to dispatch actions to the reducer
+
+import React, { useReducer } from "React";
+import uuid from 'uuid'
+import ContactContext from './ContactContext'
+import ContactReducer from './ContactReducer'
+
