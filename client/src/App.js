1e4c09e51a9b7552096e19060f8128c17e5d8b8e
Onix <gotnix13@gmail.com>
2021-07-25 14:57:12 -0400

    Added navbar layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/layout/Navbar'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <Navbar />
       My app fool!
     </div>
   );
