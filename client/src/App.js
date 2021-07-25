657e3b59b6902fad48993f2b48c0eaa6228d192d
Onix <gotnix13@gmail.com>
2021-07-25 15:11:11 -0400

    Added home and about pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
-import Navbar from './components/layout/Navbar'
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Fragment } from "react";
+import Home from './components/pages/Home'
+import About from './components/pages/About'
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      My app fool!
-    </div>
+    <Router>
+      <Fragment className='App'>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
