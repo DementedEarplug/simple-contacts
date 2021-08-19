4c3a6d2497b0496a5c06b72c82e3d084e0396d6a
Onix <gotnix13@gmail.com>
2021-08-18 20:58:33 -0400

    Added contacts and contacts items to the home component.

---
+++ client/src/components/contacts/ContactItem.jsx
@@ -0,0 +1,38 @@
+import React, { Fragment } from "react";
+
+const ContactItem = ({ contact }) => {
+  console.log(contact.contact);
+  const { name, id, phone, email, type } = contact;
+  return (
+    <div className='card bg-light'>
+      <h3 className='text-primary text-left'>
+        {name + " "}
+        <span
+          style={{ float: "right" }}
+          className={
+            "badge " +
+            (type === "professional" ? "badge-success" : "badge-primary")
+          }
+        >
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </span>
+      </h3>
+      <ul className="list" >
+        {phone && <li>
+          <i className="fas fa-phone p" ></i>
+          {phone}
+        </li>}
+        {email && <li>
+          <i className="fas fa-envelope-open p" ></i>
+          {email}
+        </li>}
+      </ul>
+      <p>
+        <button className="btn btn-sm btn-dark" >Edit</button>
+        <button className="btn btn-sm btn-danger" >Delete</button>
+      </p>
+    </div>
+  );
+};
+
+export default ContactItem;
